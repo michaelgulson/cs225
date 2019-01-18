0a575e53ad8b30a3e8e7d696b465d3c1181b7a89
Michael Gulson <mgulson2@linux-12.ews.illinois.edu>
2019-01-18 14:35:38 -0600

    my message

--- lab_intro/cs225/HSLAPixel.cpp
+++ lab_intro/cs225/HSLAPixel.cpp
@@ -14,8 +14,8 @@ namespace cs225 {
               HSLAPixel::HSLAPixel(){
                   h=0.0;
                   s=0.0;
-                  l=0.0;
-                  a=0.0;
+                  l=1.0;
+                  a=1.0;
               }
 
               HSLAPixel::HSLAPixel(double hue, double saturation, double luminance){
