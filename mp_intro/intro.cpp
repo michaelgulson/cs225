bd154d033b1286fbf79b1bb8e4c529ad0c85a40f
Michael Gulson <mgulson2@linux-09.ews.illinois.edu>
2019-01-24 15:20:27 -0600

    message

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -7,8 +7,9 @@ using cs225::PNG;
 
 void rotate(std::string inputFile, std::string outputFile) {
   // TODO: Part 2
-  cs225::PNG png, png2;
+  cs225::PNG png;
   png.readFromFile(inputFile);
+  cs225::PNG png2(png);
   for (unsigned x = 0; x < png.width(); x++) {
    for (unsigned y = 0; y < png.height(); y++) {
      HSLAPixel & pixelInput = png.getPixel(x, y);
@@ -27,6 +28,30 @@ void rotate(std::string inputFile, std::string outputFile) {
 cs225::PNG myArt(unsigned int width, unsigned int height) {
   cs225::PNG png(width, height);
   // TODO: Part 3
+  for (unsigned x = 0; x < png.width(); x++) {
+   for (unsigned y = 0; y < png.height(); y++) {
+     HSLAPixel & pixelInput = png.getPixel(x, y);
+        if(x==y){
+          pixelInput.h=240;
+          pixelInput.s=1;
+          pixelInput.l=.25;
+          pixelInput.a=1.0;
+        }
+        else if((x<=y+2)&&(x>=y-2)){
+          pixelInput.h=180;
+          pixelInput.s=1;
+          pixelInput.l=.25;
+          pixelInput.a=1.0;
+
+        }
+        else{
+          pixelInput.h=0;
+          pixelInput.s=1;
+          pixelInput.l=.25;
+          pixelInput.a=1.0;
+        }
+    }
+  }
 
   return png;
 }
