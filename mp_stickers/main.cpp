3ce49ae343379fd2c74724e8ee3cfedd7447083d
Michael Gulson <msgulson@gmail.com>
2021-06-23 18:51:15 -0400

    testing

--- mp_stickers/main.cpp
+++ mp_stickers/main.cpp
@@ -61,8 +61,8 @@ else{
 }
 */
 Image result;
-Image grainger;
-grainger.readFromFile("tests/Grainger.png");
+// Image grainger;
+// grainger.readFromFile("tests/Grainger.png");
 Image michael;
 michael.readFromFile("tests/MichaelGulson150.png");
 Image aws;
@@ -70,13 +70,13 @@ aws.readFromFile("tests/AWS-logo-150.png");
 // Image python;
 // python.readFromFile("tests/python-logo-150.png");
 
-StickerSheet s1(grainger, 5);
-s1.addSticker(michael, 150, 5);
-s1.addSticker(aws, 450, 5);
+// StickerSheet s1(grainger, 5);
+// s1.addSticker(michael, 150, 5);
+// s1.addSticker(aws, 450, 5);
 // s1.addSticker(python, 750, 5);
 
-result=s1.render();
-result.writeToFile("grainger-result.png");
+// result=s1.render();
+// result.writeToFile("grainger-result.png");
 
 // StickerSheet s2(s1);
 // s2.removeSticker(1);
